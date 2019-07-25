6b18984f740898a9231a2ef9696cfeef662438c0
gaozhihan <gaozhihan@vip.qq.com>
2019-07-25 12:30:14 +0800

    fix for resize

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
 from os import listdir, path, system, makedirs, remove
 from shutil import rmtree
-
-in_path = "in"
-tmp_path = "tmp"
-out_path = "out"
-external_path = "external"
-
-try:
-	rmtree(tmp_path)
-except:
-	pass
-makedirs(tmp_path, exist_ok=True)
-
-file_count = 0
-
-for _file in listdir(in_path):
-	if file_count%10 == 0:
-		print(_file)
-	if path.isfile(path.join(in_path, _file)):
-		if _file.split(".")[-1].lower() != "ppm":
-			# convert to ppm
-			system(path.join(external_path, "convert.exe" + " " + path.join(in_path, _file)
-							 + " " + path.join(tmp_path, _file.split(".")[0] + ".ppm")))
-
-		# apply MLAA
-		system(path.join(external_path, "mlaa.exe" + " " + path.join(tmp_path, _file.split(".")[0] + ".ppm")
-						 + " " + path.join(tmp_path, _file.split(".")[0] + "_AA.ppm")))
-
-		# delete temp ppm file
-		remove(path.join(tmp_path, _file.split(".")[0] + ".ppm"))
-
-		# convert to png
-		system(path.join(external_path, "convert.exe" + " " + path.join(tmp_path, _file.split(".")[0] + "_AA.ppm")
-						 + " " + path.join(out_path, _file.split(".")[0] + ".png")))
-
-		# delete temp AA ppm file
-		remove(path.join(tmp_path, _file.split(".")[0] + "_AA.ppm"))
-
-		file_count += 1
+import cv2
 
 
+def main():
+    in_path = "in"
+    tmp_path = "tmp"
+    out_path = "out"
+    external_path = "external"
+
+    try:
+        rmtree(tmp_path)
+    except:
+        pass
+    makedirs(tmp_path, exist_ok=True)
+
+    file_count = 0
+
+    for _file in listdir(in_path):
+        if file_count % 10 == 0:
+            print(_file)
+        input_file = path.join(in_path, _file)
+        if path.isfile(input_file):
+            if _file.split(".")[-1].lower() != "ppm":
+                # convert to ppm
+                system(path.join(external_path, "convert.exe" + " " + input_file
+                                 + " " + path.join(tmp_path, _file.split(".")[0] + ".ppm")))
+            src_image = cv2.imread(input_file, -1)
+            # apply MLAA
+            system(path.join(external_path, "mlaa.exe" + " " + path.join(tmp_path, _file.split(".")[0] + ".ppm")
+                             + " " + path.join(tmp_path, _file.split(".")[0] + "_AA.ppm")))
+            # delete temp ppm file
+            remove(path.join(tmp_path, _file.split(".")[0] + ".ppm"))
+            # convert to png
+            save_file = path.join(out_path, _file.split(".")[0] + ".png")
+            system(path.join(external_path, "convert.exe" + " " + path.join(tmp_path, _file.split(".")[0] + "_AA.ppm")
+                             + " " + save_file))
+            dst_image = cv2.imread(save_file, -1)
+            if src_image.shape != dst_image.shape:
+                h, w = src_image.shape
+                image_resize = cv2.resize(dst_image, (w, h), interpolation=cv2.INTER_CUBIC)
+                cv2.imwrite(save_file, image_resize)
+            # delete temp AA ppm file
+            remove(path.join(tmp_path, _file.split(".")[0] + "_AA.ppm"))
+
+            file_count += 1
 
 
+if __name__ == '__main__':
+    main()
